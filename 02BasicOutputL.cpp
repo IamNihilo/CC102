98084cfe0de501c02868d84d806742575d7eb8d7
Valentin <infixinityy@gmail.com>
2024-10-04 09:59:21 +0800

    Add files via upload

---
+++ 02BasicOutputL.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	
+	string School, Department, CnY, Fullname, Address;
+	
+	cout << "School : ";
+	cin >> School;
+	
+	cout << "Department : ";
+	cin >> Department;
+	
+	cout << "Course & Year Level : ";
+	cin >> CnY;
+	
+	cout << "Your Full Name : ";
+	cin >> Fullname;
+	
+	cout << "Your Permament Address : ";
+	cin >> Address;
+	
+	
+	
+}
