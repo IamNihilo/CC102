8e2d3ad9a6f869c5aa26b2b42a7520e85b1598e0
Keanu(Luca)Cereno <infixinityy@gmail.com>
2024-10-07 14:29:39 +0800

    Add files via upload

---
+++ 04BasicInputL.cpp
@@ -0,0 +1,20 @@
+#include <oistream>
+
+using namespace std;
+
+	int main () {
+		
+		string Nickname, CnY, School;
+		
+		cout << "Enter your Nickname :"; 
+		cin >> Nickname;
+		
+		cout << "Enter your Course-Year Level :";
+		cin >> CnY;
+		
+		cout << "Enter your School :";
+		cin >> School;
+		
+		cout << "WOW! ! Congrats\t" << Nickname << CnY << "\t It is a nice course. And you are studying in\t" << School << "\tWhich is on the Cneter of Exellence in Tertiary Education";
+		
+	}
