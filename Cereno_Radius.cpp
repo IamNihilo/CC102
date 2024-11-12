6953acdffb0d8e41f783eff901685177a6e28455
Keanu(Luca)Cereno <infixinityy@gmail.com>
2024-11-12 14:34:27 +0800

    Add files via upload

---
+++ Cereno_Radius.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main () {
+	
+	// constant declaration
+	const double P1 = 3.14159;
+	// variable delcarations
+	double radius;
+	double area;
+	
+	// assignment statements
+	cout << "Enter circle radius ";
+	cin >> radius;
+	area = P1 * radius * radius;
+	cout << "Area	: " << area << endl;
+	
+	return 0;
+}
+
+// guys tama ba tong code na to?
