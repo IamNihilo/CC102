2bedcca1a1a63fb93fed7bbbdcd45ebbbae5a1bb
Keanu(Luca)Cereno <infixinityy@gmail.com>
2024-11-30 23:52:23 +0800

    Add files via upload

---
+++ ACT1_B_CERENO.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+{ 
+	int row = 8;
+	
+	for (int index = 8; index >= 1; index--) {
+		for (int index2 = 1; index2 <= index; index2++) {
+			{ cout << "# ";
+			
+			}
+		}cout << endl;
+	}
+	
+	return 0;
+}
